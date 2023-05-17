0be124910dbc1105df30fa9e1a7d78f10c8cae71
sebasgr10 <sebasgrgr10@gmail.com>
2023-05-17 20:51:35 +0200

    wasiiiii

--- app/src/main/java/com/example/cityfootie_compose/navigation/AppScreens.kt
+++ app/src/main/java/com/example/cityfootie_compose/navigation/AppScreens.kt
@@ -1,6 +1,7 @@
 package com.example.cityfootie_compose.navigation
 
 sealed class AppScreens(val route: String) {
+    //WAAAAASIIIIII
     object SplashScreen : AppScreens("splash_screen")
     object MainScreen : AppScreens("main_screen")
     object LoginScreen : AppScreens("login_screen")
