a8be2c6808b226cc2b08d2bcbc95a191af666c10
Dixamo <juandarm02@gmail.com>
2023-06-07 00:07:06 +0200

    Ahora se muestran los usuarios apuntados

--- app/src/main/java/com/example/cityfootie_compose/ui/screens/join_to_football_match/JoinToFootballMatchScreen.kt
+++ app/src/main/java/com/example/cityfootie_compose/ui/screens/join_to_football_match/JoinToFootballMatchScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -195,9 +196,9 @@ fun UserInfo(player: Player) {
                 color = MaterialTheme.colors.primary,
                 style = MaterialTheme.typography.subtitle2,
             )
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.width(16.dp))
             Text(
-                text = player.email!!,
+                text = player.name!!,
                 color = MaterialTheme.colors.onBackground,
                 style = MaterialTheme.typography.subtitle2,
             )
